9e6e47b555ba82503f25f99869bbb6e352d8507e
sampeteq <sampeteq@gmail.com>
2022-11-25 03:38:19 +0100

    Removed useless method

--- src/main/java/code/films/FilmRepository.java
+++ src/main/java/code/films/FilmRepository.java
@@ -2,10 +2,8 @@ package code.films;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 interface FilmRepository extends JpaRepository<Film, UUID> {
 
-    List<Film> findByCategory(FilmCategory category);
 }

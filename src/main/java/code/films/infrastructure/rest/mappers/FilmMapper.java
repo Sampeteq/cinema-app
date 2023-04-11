432be6977d714cc4c331efd51b51edca4c02e7a5
sampeteq <sampeteq@gmail.com>
2023-04-11 21:17:32 +0200

    Added MapStruct

--- src/main/java/code/films/infrastructure/rest/mappers/FilmMapper.java
+++ src/main/java/code/films/infrastructure/rest/mappers/FilmMapper.java
@@ -1,19 +1,11 @@
 package code.films.infrastructure.rest.mappers;
 
-import code.films.infrastructure.rest.dto.FilmDto;
 import code.films.domain.Film;
-import org.springframework.stereotype.Component;
+import code.films.infrastructure.rest.dto.FilmDto;
+import org.mapstruct.Mapper;
 
-@Component
-public class FilmMapper {
+@Mapper(componentModel = "spring")
+public interface FilmMapper {
 
-    public FilmDto mapToDto(Film film) {
-        return new FilmDto(
-                film.getId(),
-                film.getTitle(),
-                film.getCategory(),
-                film.getYear(),
-                film.getDurationInMinutes()
-        );
-    }
+    FilmDto mapToDto(Film film);
 }
--- src/main/java/code/films/infrastructure/rest/mappers/ScreeningMapper.java
+++ src/main/java/code/films/infrastructure/rest/mappers/ScreeningMapper.java
@@ -1,20 +1,40 @@
 package code.films.infrastructure.rest.mappers;
 
-import code.films.infrastructure.rest.dto.ScreeningDto;
-import code.films.domain.Screening;
 import code.bookings.domain.Seat;
-import org.springframework.stereotype.Component;
+import code.films.domain.Film;
+import code.films.domain.Screening;
+import code.films.infrastructure.rest.dto.ScreeningDto;
+import code.rooms.domain.Room;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
-@Component
-public class ScreeningMapper {
-    public ScreeningDto mapToDto(Screening screening) {
-        return new ScreeningDto(
-                screening.getId(),
-                screening.getDate(),
-                screening.getMinAge(),
-                screening.getFilm().getId(),
-                screening.getRoom().getId(),
-                (int) screening.getSeats().stream().filter(Seat::isFree).count()
-        );
+import java.util.List;
+import java.util.UUID;
+
+@Mapper(componentModel = "spring")
+public interface ScreeningMapper {
+
+    @Mapping(target = "filmId", source = "film", qualifiedByName = "filmToUuid")
+    @Mapping(target = "roomId", source = "room", qualifiedByName = "roomToUuid")
+    @Mapping(target = "freeSeats", source = "seats", qualifiedByName = "seatsToFreeSeats")
+    ScreeningDto mapToDto(Screening screening);
+
+    @Named("filmToUuid")
+    static UUID filmToUuid(Film film) {
+        return film.getId();
+    }
+
+    @Named("roomToUuid")
+    static UUID roomToUuid(Room room) {
+        return room.getId();
+    }
+
+    @Named("seatsToFreeSeats")
+    static int seatsToFreeSeats(List<Seat> seats) {
+        return (int) seats
+                .stream()
+                .filter(Seat::isFree)
+                .count();
     }
 }

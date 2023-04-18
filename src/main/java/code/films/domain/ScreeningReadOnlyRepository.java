4d7cd23bcd7fc21de041da75902885e23e30042d
sampeteq <sampeteq@gmail.com>
2023-04-18 20:24:37 +0200

    Changed searching screenings by params from JPQL query to Specifications

--- src/main/java/code/films/domain/ScreeningReadOnlyRepository.java
+++ src/main/java/code/films/domain/ScreeningReadOnlyRepository.java
@@ -1,23 +1,11 @@
 package code.films.domain;
 
-import code.films.domain.queries.GetScreeningsQuery;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ScreeningReadOnlyRepository extends Repository<Screening, UUID> {
-
+public interface ScreeningReadOnlyRepository extends Repository<Screening, UUID>, JpaSpecificationExecutor<Screening> {
     Optional<Screening> getById(UUID id);
-
-    @Query("select distinct s from Screening s " +
-            "left join fetch s.seats left join fetch s.film left join fetch s.room " +
-            "where (cast(cast(:#{#params.date} as string) as timestamp) is null " +
-            "or s.date = cast(cast(:#{#params.date} as string) as timestamp)) " +
-            "and (:#{#params.filmId} is null or s.film.id = :#{#params.filmId})")
-    List<Screening> getBy(GetScreeningsQuery params);
-
-
 }

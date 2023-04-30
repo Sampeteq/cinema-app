f7e45d1310f235d94841ffdd945457ea5ae1e1ff
sampeteq <sampeteq@gmail.com>
2023-04-30 13:51:27 +0200

    Db migration fix

--- src/main/resources/database/08-drop-column-number-and-create-column-custom_id-in-rooms-table.sql
+++ src/main/resources/database/08-drop-column-number-and-create-column-custom_id-in-rooms-table.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 --changeset nofenak:7
 alter table if exists rooms drop column if exists number;
-alter table if exists rooms add column custom_id varchar(255)
+alter table if exists rooms add column if not exists custom_id varchar(255)

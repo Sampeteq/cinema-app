fb9d23c5ce063921586523fa2d9eb967faa06095
sampeteq <sampeteq@gmail.com>
2023-06-11 19:49:37 +0200

    Removed useless classes

--- src/main/java/code/films/application/queries/FilmScreeningReadQuery.java
+++
@@ -1,8 +0,0 @@
-package code.films.application.queries;
-
-import lombok.Builder;
-
-import java.time.LocalDateTime;
-
-@Builder
-public record FilmScreeningReadQuery(String filmTitle, LocalDateTime date) {}
--- src/main/java/code/films/application/queries/FilmScreeningSeatQuery.java
+++
@@ -1,7 +0,0 @@
-package code.films.application.queries;
-
-import lombok.Builder;
-
-@Builder
-public record FilmScreeningSeatQuery(Long screeningId) {
-}

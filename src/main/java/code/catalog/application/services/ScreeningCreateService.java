03907aa61387f49f2dc7aa53745b4787a1cd1bd1
sampeteq <sampeteq@gmail.com>
2023-07-02 17:24:21 +0200

    ScreeningCreateService fixes

--- src/main/java/code/catalog/application/services/ScreeningCreateService.java
+++ src/main/java/code/catalog/application/services/ScreeningCreateService.java
@@ -9,7 +9,7 @@ import code.catalog.domain.services.ScreeningDateValidateService;
 import code.shared.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Clock;
 
@@ -19,27 +19,23 @@ public class ScreeningCreateService {
 
     private final ScreeningDateValidateService screeningDateValidateService;
     private final Clock clock;
-    private final TransactionTemplate transactionTemplate;
     private final FilmRepository filmRepository;
     private final RoomAvailableService roomAvailableService;
     private final SeatFactory seatFactory;
 
-    public Long createScreening(ScreeningCreateDto dto) {
+    @Transactional
+    public void createScreening(ScreeningCreateDto dto) {
         screeningDateValidateService.validate(dto.date(), clock);
-        var screening = transactionTemplate.execute(status -> {
-            var film = getFilmOrThrow(dto.filmId());
-            var screeningDate = dto.date();
-            var screeningFinishDate = dto.date().plusMinutes(film.getDurationInMinutes());
-            var availableRoom = roomAvailableService.getFirstAvailableRoom(
-                    screeningDate,
-                    screeningFinishDate
-            );
-            var seats = seatFactory.createSeats(availableRoom);
-            var newScreening = Screening.create(dto.date(), film, availableRoom, seats);
-            film.addScreening(newScreening);
-            return newScreening;
-        });
-        return screening.getId();
+        var film = getFilmOrThrow(dto.filmId());
+        var screeningDate = dto.date();
+        var screeningFinishDate = dto.date().plusMinutes(film.getDurationInMinutes());
+        var availableRoom = roomAvailableService.getFirstAvailableRoom(
+                screeningDate,
+                screeningFinishDate
+        );
+        var seats = seatFactory.createSeats(availableRoom);
+        var newScreening = Screening.create(dto.date(), film, availableRoom, seats);
+        film.addScreening(newScreening);
     }
 
     private Film getFilmOrThrow(Long filmId) {

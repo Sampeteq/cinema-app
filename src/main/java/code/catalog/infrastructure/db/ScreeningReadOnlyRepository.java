a64f6729e681cdd02111af2c3554a029ce7446d5
sampeteq <sampeteq@gmail.com>
2023-07-12 00:52:24 +0200

    ScreeningReadOnlyRepository methods order fix

--- src/main/java/code/catalog/infrastructure/db/ScreeningReadOnlyRepository.java
+++ src/main/java/code/catalog/infrastructure/db/ScreeningReadOnlyRepository.java
@@ -11,6 +11,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ScreeningReadOnlyRepository extends Repository<Screening, Long> {
+    Optional<Screening> findById(Long id);
+
+    @Query("select s from Screening s join fetch s.seats se where se.id = :seatId")
+    Optional<Screening> findBySeatId(@Param("seatId") Long seatId);
+
     List<Screening> findAll();
 
     List<Screening> findByFilm_Title(String filmTitle);
@@ -18,11 +23,6 @@ public interface ScreeningReadOnlyRepository extends Repository<Screening, Long>
     List<Screening> findByFilm_Category(FilmCategory filmCategory);
 
     List<Screening> findByDateBetween(LocalDateTime from, LocalDateTime to);
-
-    Optional<Screening> findById(Long id);
-
-    @Query("select s from Screening s join fetch s.seats se where se.id = :seatId")
-    Optional<Screening> findBySeatId(@Param("seatId") Long seatId);
 
     @Query("select s from Screening s where s.endDate < CURRENT_DATE")
     List<Screening> findEnded();

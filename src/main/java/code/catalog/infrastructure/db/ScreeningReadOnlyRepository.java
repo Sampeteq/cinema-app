b4f01c74b49d6db1b856ad4d0d4e89aa8ec4ce7c
sampeteq <sampeteq@gmail.com>
2023-07-12 17:03:37 +0200

    Removed useless method from ScreeningReadOnlyRepository

--- src/main/java/code/catalog/infrastructure/db/ScreeningReadOnlyRepository.java
+++ src/main/java/code/catalog/infrastructure/db/ScreeningReadOnlyRepository.java
@@ -4,7 +4,6 @@ import code.catalog.domain.FilmCategory;
 import code.catalog.domain.Screening;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,9 +11,6 @@ import java.util.Optional;
 
 public interface ScreeningReadOnlyRepository extends Repository<Screening, Long> {
     Optional<Screening> findById(Long id);
-
-    @Query("select s from Screening s join fetch s.seats se where se.id = :seatId")
-    Optional<Screening> findBySeatId(@Param("seatId") Long seatId);
 
     List<Screening> findAll();
 

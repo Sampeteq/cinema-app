735231911c4dffa487fa31925e04442d3cc35c1b
sampeteq <sampeteq@gmail.com>
2023-07-26 03:16:48 +0200

    Removed useless fields from bookings details db migration

--- src/main/resources/database/08-create-table-bookings-details.sql
+++ src/main/resources/database/08-create-table-bookings-details.sql
@@ -2,8 +2,6 @@ create table if not exists bookings_details(
     id bigint primary key generated always as identity not null,
     film_title varchar,
     room_custom_id varchar,
-    seat_row_number int,
-    seat_number int,
     booking_id bigint,
     foreign key (booking_id) references bookings (id)
 )

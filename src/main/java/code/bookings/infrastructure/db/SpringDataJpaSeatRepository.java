7b5340f306344c8884f86eb7602232400fca1f58
sampeteq <sampeteq@gmail.com>
2023-08-13 13:23:45 +0200

    Changed reading Seat by id to use join (reduce SQL queries)

--- src/main/java/code/bookings/infrastructure/db/SpringDataJpaSeatRepository.java
+++ src/main/java/code/bookings/infrastructure/db/SpringDataJpaSeatRepository.java
@@ -4,6 +4,8 @@ import code.bookings.domain.Seat;
 import code.bookings.domain.ports.SeatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,10 +18,12 @@ public class SpringDataJpaSeatRepository implements SeatRepository {
 
     @Override
     public Optional<Seat> readById(Long id) {
-        return jpaSeatRepository.findById(id);
+        return jpaSeatRepository.readByIdWithScreening(id);
     }
 }
 
 interface JpaSeatRepository extends JpaRepository<Seat, Long> {
 
+    @Query("select s from Seat s left join fetch s.screening where s.id = :id")
+    Optional<Seat> readByIdWithScreening(@Param("id") Long id);
 }

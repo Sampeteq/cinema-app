a741ae0888291fdedc2980de6dc25b8f81e6fc51
sampeteq <sampeteq@gmail.com>
2023-08-15 23:01:36 +0200

    Class name fix

--- src/test/java/code/bookings/domain/BookingTests.java
+++ src/test/java/code/bookings/domain/BookingTests.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class BookingUT {
+class BookingTests {
 
     private static final LocalDateTime currentDate = new MockTimeProvider().getCurrentDate();
 

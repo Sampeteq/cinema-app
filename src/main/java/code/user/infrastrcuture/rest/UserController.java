85837933a9692fe8a4c898a85e328bae967dbe50
sampeteq <sampeteq@gmail.com>
2023-08-21 02:24:42 +0200

    UserController fixes

--- src/main/java/code/user/infrastrcuture/rest/UserController.java
+++ src/main/java/code/user/infrastrcuture/rest/UserController.java
@@ -6,10 +6,10 @@ import code.user.application.dto.UserSignUpDto;
 import code.user.application.services.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -21,33 +21,29 @@ class UserController {
     private final UserFacade userFacade;
 
     @PostMapping("/signup")
-    ResponseEntity<String> signUpUser(@RequestBody @Valid UserSignUpDto dto) {
+    @ResponseStatus(HttpStatus.CREATED)
+    void signUpUser(@RequestBody @Valid UserSignUpDto dto) {
         userFacade.signUpUser(dto);
-        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PostMapping("/signin")
-    ResponseEntity<String> signInUser(@RequestBody UserSignInDto dto) {
+    void signInUser(@RequestBody UserSignInDto dto) {
         userFacade.signInUser(dto);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/signout")
-    ResponseEntity<String> signOutUser() {
+    void signOutUser() {
         userFacade.signOutUser();
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/password/reset")
-    ResponseEntity<String> resetUserPassword(@RequestParam String mail) {
+    void resetUserPassword(@RequestParam String mail) {
         userFacade.resetUserPassword(mail);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/password/new")
-    ResponseEntity<String> setNewUserPassword(@RequestBody UserPasswordNewDto dto) {
+    void setNewUserPassword(@RequestBody UserPasswordNewDto dto) {
         userFacade.setNewUserPassword(dto);
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
 

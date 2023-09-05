afcbdcbe27b8d3eba15329e3884fbca96b0e5f42
sampeteq <sampeteq@gmail.com>
2023-09-05 14:33:04 +0200

    Added "CURRENT_YEAR" constant to FilmTestHelper

--- src/test/java/code/catalog/FilmTestHelper.java
+++ src/test/java/code/catalog/FilmTestHelper.java
@@ -9,18 +9,19 @@ import java.util.List;
 
 public final class FilmTestHelper {
 
+    public static final int CURRENT_YEAR = Year.now().getValue();
+
     private FilmTestHelper() {
     }
 
     public static FilmCreateDto createFilmCreateDto() {
         var title = "Test film 1";
         var category = FilmCategory.COMEDY;
-        var year = Year.now().getValue();
         var duration = 100;
         return new FilmCreateDto(
                 title,
                 category,
-                year,
+                CURRENT_YEAR,
                 duration
         );
     }
@@ -28,36 +29,33 @@ public final class FilmTestHelper {
     public static Film createFilm() {
         var title = "Test film 1";
         var category = FilmCategory.COMEDY;
-        var year = Year.now().getValue();
         var duration = 100;
         return Film.create(
                 title,
                 category,
-                year,
+                CURRENT_YEAR,
                 duration
         );
     }
 
     public static Film createFilm(FilmCategory filmCategory) {
         var title = "Test film 1";
-        var year = Year.now().getValue();
         var duration = 100;
         return Film.create(
                 title,
                 filmCategory,
-                year,
+                CURRENT_YEAR,
                 duration
         );
     }
 
     public static Film createFilm(String filmTitle) {
         var category = FilmCategory.COMEDY;
-        var year = Year.now().getValue();
         var duration = 100;
         return Film.create(
                 filmTitle,
                 category,
-                year,
+                CURRENT_YEAR,
                 duration
         );
     }
@@ -65,17 +63,16 @@ public final class FilmTestHelper {
     public static List<Film> createFilms() {
         var title1 = "Test film 1";
         var category1 = FilmCategory.COMEDY;
-        var year1 = Year.now().getValue();
         var duration1 = 100;
 
         var title2 = "Test film 2";
         var category2 = FilmCategory.DRAMA;
-        var year2 = Year.now().getValue() + 1;
+        var year2 = CURRENT_YEAR + 1;
         var duration2 = 120;
         var film1 = Film.create(
                 title1,
                 category1,
-                year1,
+                CURRENT_YEAR,
                 duration1
         );
         var film2 = Film.create(
@@ -89,17 +86,16 @@ public final class FilmTestHelper {
 
     public static List<Film> createFilms(FilmCategory filmCategory) {
         var title1 = "Test film 1";
-        var year1 = Year.now().getValue();
         var duration1 = 100;
 
         var title2 = "Test film 2";
-        var year2 = Year.now().getValue() + 1;
+        var year2 = CURRENT_YEAR + 1;
         var duration2 = 120;
 
         var film1 = Film.create(
                 title1,
                 filmCategory,
-                year1,
+                CURRENT_YEAR,
                 duration1
         );
         var film2 = Film.create(
@@ -112,7 +108,7 @@ public final class FilmTestHelper {
     }
 
     public static List<Integer> getWrongFilmYears() {
-        var currentYear = Year.now().getValue();
+        var currentYear = CURRENT_YEAR;
         return List.of(
                 currentYear - 2,
                 currentYear + 2

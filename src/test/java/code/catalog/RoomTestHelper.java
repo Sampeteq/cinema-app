f0d53fc8e83dc10907a636698456c22cff004dcb
sampeteq <sampeteq@gmail.com>
2023-09-05 14:49:51 +0200

    Added sample arguments constants to RoomTestHelper

--- src/test/java/code/catalog/RoomTestHelper.java
+++ src/test/java/code/catalog/RoomTestHelper.java
@@ -4,17 +4,20 @@ import code.catalog.domain.Room;
 
 public final class RoomTestHelper {
 
+    private static final String CUSTOM_ID = "1";
+
+    private static final int ROWS_NUMBER = 15;
+
+    private static final int ROW_SEATS_NUMBER = 10;
+
     private RoomTestHelper() {
     }
 
     public static Room createRoom() {
-        var customId = "1";
-        var rowsNumber = 15;
-        var rowSeatsNumber = 10;
         return Room.create(
-                customId,
-                rowsNumber,
-                rowSeatsNumber
+                CUSTOM_ID,
+                ROWS_NUMBER,
+                ROW_SEATS_NUMBER
         );
     }
 }

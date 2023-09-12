8febf7c4a144cd5979060061d83920320dc475ee
sampeteq <sampeteq@gmail.com>
2023-09-12 03:36:34 +0200

    Removed useless method

--- src/main/java/code/catalog/application/services/FilmReadService.java
+++ src/main/java/code/catalog/application/services/FilmReadService.java
@@ -2,17 +2,12 @@ package code.catalog.application.services;
 
 import code.catalog.application.dto.FilmDto;
 import code.catalog.application.dto.FilmMapper;
-import code.catalog.application.dto.ScreeningDto;
-import code.catalog.application.dto.ScreeningMapper;
-import code.catalog.domain.Screening;
 import code.catalog.domain.ports.FilmRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
-import static java.util.Comparator.comparing;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,7 +16,6 @@ class FilmReadService {
 
     private final FilmRepository filmRepository;
     private final FilmMapper filmMapper;
-    private final ScreeningMapper screeningMapper;
 
     public List<FilmDto> readAll() {
         return filmRepository
@@ -29,17 +23,5 @@ class FilmReadService {
                 .stream()
                 .map(filmMapper::mapToDto)
                 .toList();
-    }
-
-    public List<ScreeningDto> readAllScreenings() {
-        return filmRepository
-                .readAll()
-                .stream()
-                .flatMap(film -> film
-                        .getScreenings()
-                        .stream()
-                        .sorted(comparing(Screening::getDate))
-                        .map(screeningMapper::mapToDto)
-                ).toList();
     }
 }

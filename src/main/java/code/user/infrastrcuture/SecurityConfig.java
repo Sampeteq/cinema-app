4e873cf62852c777e49578b77e2fa029beff8a1f
sampeteq <sampeteq@gmail.com>
2023-09-14 13:26:13 +0200

    Removed useless bean

--- src/main/java/code/user/infrastrcuture/SecurityConfig.java
+++ src/main/java/code/user/infrastrcuture/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -63,11 +62,5 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         return username -> userRepository
                 .readyByMail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
     }
 }

9f2202ef2166978e015da0dc79d26cf1f9932e6a
sampeteq <sampeteq@gmail.com>
2023-10-03 00:37:34 +0200

    ScreeningEndServiceIT fixes

--- src/test/java/com/cinema/catalog/application/services/ScreeningEndServiceIT.java
+++ src/test/java/com/cinema/catalog/application/services/ScreeningEndServiceIT.java
@@ -1,21 +1,24 @@
 package com.cinema.catalog.application.services;
 
 import com.cinema.SpringIT;
-import com.cinema.catalog.ScreeningTestHelper;
 import com.cinema.catalog.domain.FilmRepository;
 import com.cinema.catalog.domain.ScreeningReadOnlyRepository;
 import com.cinema.rooms.application.services.RoomFacade;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+
 import static com.cinema.catalog.FilmTestHelper.createFilm;
+import static com.cinema.catalog.ScreeningTestHelper.createScreening;
 import static com.cinema.tickets.TicketTestHelper.createRoomCreateDto;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ScreeningEndServiceIT extends SpringIT {
 
     @Autowired
-    private ScreeningEndService screeningFinishHandler;
+    private ScreeningEndService screeningEndService;
 
     @Autowired
     private FilmRepository filmRepository;
@@ -26,21 +29,26 @@ class ScreeningEndServiceIT extends SpringIT {
     @Autowired
     private ScreeningReadOnlyRepository screeningReadOnlyRepository;
 
+    @Autowired
+    private Clock clockMock;
+
     @Test
     void shouldRemoveRoomsFromFinishedScreenings() {
         //given
         var film = filmRepository.add(createFilm());
         roomFacade.createRoom(createRoomCreateDto());
-        ScreeningTestHelper.createScreenings(film).forEach(film::addScreening);
+        var screeningDate = LocalDateTime.now(clockMock).minusSeconds(1);
+        var screening = createScreening(film, screeningDate);
+        film.addScreening(screening);
         filmRepository.add(film);
 
         //when
-        screeningFinishHandler.removeRoomsFromEndedScreenings();
+        screeningEndService.removeRoomsFromEndedScreenings();
 
         //then
         var screenings = screeningReadOnlyRepository.readAll();
         assertThat(screenings)
                 .isNotEmpty()
-                .allMatch(screening -> screening.getRoomId() == null);
+                .allMatch(it -> it.getRoomId() == null);
     }
 }

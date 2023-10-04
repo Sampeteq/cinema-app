a46c72a4751541ed1bbbc7e27cb8905fbc763998
sampeteq <sampeteq@gmail.com>
2023-10-04 12:49:35 +0200

    Formatting fix

--- src/main/java/com/cinema/users/application/services/UserSignUpService.java
+++ src/main/java/com/cinema/users/application/services/UserSignUpService.java
@@ -16,6 +16,7 @@ class UserSignUpService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
     void signUp(UserSignUpDto dto) {
         if (userRepository.existsByMail(dto.mail())) {
             throw new UserMailAlreadyExistsException();

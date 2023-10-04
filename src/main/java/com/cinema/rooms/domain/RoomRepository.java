90f5fc028ea9c0b82f4fd283fbea7c3a9daa8152
sampeteq <sampeteq@gmail.com>
2023-10-04 12:39:46 +0200

    Formatting fix

--- src/main/java/com/cinema/rooms/domain/RoomRepository.java
+++ src/main/java/com/cinema/rooms/domain/RoomRepository.java
@@ -5,12 +5,8 @@ import java.util.Optional;
 
 public interface RoomRepository {
     Room add(Room room);
-
     Optional<Room> readById(String id);
-
     List<Room> readAll();
-
     Long count();
-
     boolean existsById(String id);
 }

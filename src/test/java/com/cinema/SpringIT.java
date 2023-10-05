a42518aae9c5845fe7a871a00df7f242993511f6
sampeteq <sampeteq@gmail.com>
2023-10-05 13:30:24 +0200

    WebTestClient fixes

--- src/test/java/com/cinema/SpringIT.java
+++ src/test/java/com/cinema/SpringIT.java
@@ -4,22 +4,20 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class SpringIT {
 
     @Autowired
     private SqlDatabaseCleaner sqlDatabaseCleaner;
 
     @Autowired
-    protected WebTestClient webTestClient;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    protected WebTestClient webTestClient;
 
     @AfterEach
     void cleanDb() {

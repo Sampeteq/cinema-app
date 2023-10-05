d102804f8f15ac12ebdab0882c95f3e24c65fed5
sampeteq <sampeteq@gmail.com>
2023-10-05 04:16:54 +0200

    Removed useless methods from ScreeningTestHelper

--- src/test/java/com/cinema/catalog/ScreeningTestHelper.java
+++ src/test/java/com/cinema/catalog/ScreeningTestHelper.java
@@ -13,8 +13,6 @@ public final class ScreeningTestHelper {
     private ScreeningTestHelper() {
     }
 
-    public static final LocalDateTime SCREENING_DATE = LocalDateTime.of(2023, 12, 13, 16, 30);
-
     public static Screening createScreening(
             Film film,
             LocalDateTime screeningDate
@@ -28,25 +26,6 @@ public final class ScreeningTestHelper {
                 roomId,
                 List.of(seat)
         );
-    }
-
-    public static List<Screening> createScreenings(Film film) {
-        var isFree = true;
-        var seat = new Seat(1,2, isFree);
-        var roomId = "1";
-        var screening1 = new Screening(
-                SCREENING_DATE,
-                film,
-                roomId,
-                List.of(seat)
-        );
-        var screening2 = new Screening(
-                SCREENING_DATE.plusDays(1),
-                film,
-                roomId,
-                List.of(seat)
-        );
-        return List.of(screening1, screening2);
     }
 
     /** Difference between current and screening date must be at least 7 days */

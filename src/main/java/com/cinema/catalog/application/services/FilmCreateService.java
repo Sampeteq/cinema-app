6a13cf720f23c0ca495bad597e62bf6cf11a2c08
sampeteq <sampeteq@gmail.com>
2023-10-07 21:56:59 +0200

    Removed FilmYearValidateService and moved it logic to FilmCreateService

--- src/main/java/com/cinema/catalog/application/services/FilmCreateService.java
+++ src/main/java/com/cinema/catalog/application/services/FilmCreateService.java
@@ -4,19 +4,22 @@ import com.cinema.catalog.application.dto.FilmCreateDto;
 import com.cinema.catalog.domain.Film;
 import com.cinema.catalog.domain.FilmRepository;
 import com.cinema.catalog.domain.exceptions.FilmTitleNotUniqueException;
-import com.cinema.catalog.domain.services.FilmYearValidateService;
+import com.cinema.catalog.domain.exceptions.FilmYearOutOfRangeException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Year;
 
 @Service
 @RequiredArgsConstructor
 class FilmCreateService {
 
-    private final FilmYearValidateService filmYearValidateService;
     private final FilmRepository filmRepository;
 
     void creteFilm(FilmCreateDto dto) {
-        filmYearValidateService.validate(dto.year());
+        if (isFilmYearOutOfRange(dto.year())) {
+            throw new FilmYearOutOfRangeException();
+        }
         if (filmRepository.existsByTitle(dto.title())) {
             throw new FilmTitleNotUniqueException();
         }
@@ -27,5 +30,10 @@ class FilmCreateService {
                 dto.durationInMinutes()
         );
         filmRepository.add(film);
+    }
+
+    private static boolean isFilmYearOutOfRange(Integer year) {
+        var currentYear = Year.now().getValue();
+        return year != currentYear - 1 && year != currentYear && year != currentYear + 1;
     }
 }

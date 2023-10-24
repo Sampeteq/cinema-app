5bbd1af20b13ce2f2a09d3dcf7d9c612b45e425e
sampeteq <sampeteq@gmail.com>
2023-10-24 21:47:08 +0200

    Removed useless toJson method from SpringIT

--- src/test/java/com/cinema/SpringIT.java
+++ src/test/java/com/cinema/SpringIT.java
@@ -1,7 +1,5 @@
 package com.cinema;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,21 +12,10 @@ public abstract class SpringIT {
     private SqlDatabaseCleaner sqlDatabaseCleaner;
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
     protected WebTestClient webTestClient;
 
     @AfterEach
     void cleanDb() {
         sqlDatabaseCleaner.clean();
-    }
-
-    public String toJson(Object object) {
-        try {
-            return objectMapper.writeValueAsString(object);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
     }
 }

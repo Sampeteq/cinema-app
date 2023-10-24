306655ac7ab7c4871e0f858c8c628950085b62e8
sampeteq <sampeteq@gmail.com>
2023-10-24 21:40:32 +0200

    Added @JsonFormat to "screeningDate" field in TicketDto

--- src/main/java/com/cinema/tickets/application/dto/TicketDto.java
+++ src/main/java/com/cinema/tickets/application/dto/TicketDto.java
@@ -1,6 +1,7 @@
 package com.cinema.tickets.application.dto;
 
 import com.cinema.tickets.domain.TicketStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
@@ -8,6 +9,7 @@ public record TicketDto(
         Long id,
         TicketStatus status,
         String filmTitle,
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
         LocalDateTime screeningDate,
         String roomId,
         Integer rowNumber,

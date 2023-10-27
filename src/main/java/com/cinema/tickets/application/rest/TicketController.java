4796bd5deb38787ba3566192cf2b257d9982890e
sampeteq <sampeteq@gmail.com>
2023-10-27 17:52:04 +0200

    Added logging to bookTicket method from TicketController

--- src/main/java/com/cinema/tickets/application/rest/TicketController.java
+++ src/main/java/com/cinema/tickets/application/rest/TicketController.java
@@ -6,30 +6,35 @@ import com.cinema.tickets.application.services.TicketService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/tickets")
 @RequiredArgsConstructor
+@Slf4j
 class TicketController {
 
     private final TicketService ticketService;
 
     @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
     @SecurityRequirement(name = "basic")
-    void bookTicket(@RequestBody @Valid TicketBookDto dto) {
+    ResponseEntity<Object> bookTicket(@RequestBody @Valid TicketBookDto dto) {
+        log.info("DTO:{}", dto);
         ticketService.bookTicket(dto);
+        var responseEntity = ResponseEntity.created(URI.create("/my/tickets")).build();
+        log.info("Response entity:{}", responseEntity);
+        return responseEntity;
     }
 
     @PatchMapping("/{ticketId}/cancel")

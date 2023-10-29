4cf42837ebdb417b9179d13b82552c35206292b9
sampeteq <sampeteq@gmail.com>
2023-10-29 13:02:07 +0100

    Split TicketEventHandler into TicketBookedHandler and TicketCancelledHandler

---
+++ src/main/java/com/cinema/screenings/application/TicketBookedHandler.java
@@ -0,0 +1,31 @@
+package com.cinema.screenings.application;
+
+import com.cinema.screenings.domain.ScreeningRepository;
+import com.cinema.screenings.domain.exceptions.ScreeningNotFoundException;
+import com.cinema.screenings.domain.exceptions.SeatNotFoundException;
+import com.cinema.tickets.domain.events.TicketBookedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TicketBookedHandler {
+
+    private final ScreeningRepository screeningRepository;
+
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void handle(TicketBookedEvent event) {
+        log.info("Handled event:{}", event);
+        var seat = screeningRepository
+                .readById(event.screeningId())
+                .orElseThrow(ScreeningNotFoundException::new)
+                .findSeat(event.seatId())
+                .orElseThrow(SeatNotFoundException::new);
+        seat.take();
+        log.info("Taken seat:{}", seat);
+    }
+}
--- src/main/java/com/cinema/screenings/application/TicketCancelledHandler.java
+++ src/main/java/com/cinema/screenings/application/TicketCancelledHandler.java
@@ -1,51 +1,31 @@
 package com.cinema.screenings.application;
 
 import com.cinema.screenings.domain.ScreeningRepository;
-import com.cinema.screenings.domain.Seat;
 import com.cinema.screenings.domain.exceptions.ScreeningNotFoundException;
 import com.cinema.screenings.domain.exceptions.SeatNotFoundException;
-import com.cinema.tickets.domain.events.TicketBookedEvent;
 import com.cinema.tickets.domain.events.TicketCancelledEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-@Service
+@Component
 @RequiredArgsConstructor
 @Slf4j
-class TicketEventHandler {
+public class TicketCancelledHandler {
 
     private final ScreeningRepository screeningRepository;
 
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
-    public void handle(TicketBookedEvent event) {
-        log.info("Handled event:{}", event);
-        var seat = readSeat(
-                event.screeningId(),
-                event.seatId()
-        );
-        seat.take();
-        log.info("Taken seat:{}", seat);
-    }
-
-    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void handle(TicketCancelledEvent event) {
         log.info("Handled event:{}", event);
-        var seat = readSeat(
-                event.screeningId(),
-                event.seatId()
-        );
+        var seat = screeningRepository
+                .readById(event.screeningId())
+                .orElseThrow(ScreeningNotFoundException::new)
+                .findSeat(event.seatId())
+                .orElseThrow(SeatNotFoundException::new);
         seat.free();
         log.info("Freed seat:{}", seat);
-    }
-
-    private Seat readSeat(Long screeningId, Long seatId) {
-        return screeningRepository
-                .readById(screeningId)
-                .orElseThrow(ScreeningNotFoundException::new)
-                .findSeat(seatId)
-                .orElseThrow(SeatNotFoundException::new);
     }
 }

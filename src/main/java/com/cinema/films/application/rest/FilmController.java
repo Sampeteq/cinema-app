2e5a097e1edcd9295275969fb63e336d8ec64c37
sampeteq <sampeteq@gmail.com>
2023-10-30 10:31:05 +0100

    FilmController fix

--- src/main/java/com/cinema/films/application/rest/FilmController.java
+++ src/main/java/com/cinema/films/application/rest/FilmController.java
@@ -2,17 +2,16 @@ package com.cinema.films.application.rest;
 
 import com.cinema.films.application.commands.CreateFilm;
 import com.cinema.films.application.commands.DeleteFilm;
-import com.cinema.films.application.queries.dto.FilmDto;
 import com.cinema.films.application.commands.handlers.CreateFilmHandler;
 import com.cinema.films.application.commands.handlers.DeleteFilmHandler;
-import com.cinema.films.application.queries.handlers.ReadFilmsHandler;
 import com.cinema.films.application.queries.ReadFilms;
+import com.cinema.films.application.queries.dto.FilmDto;
+import com.cinema.films.application.queries.handlers.ReadFilmsHandler;
 import com.cinema.films.domain.FilmCategory;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -48,12 +46,14 @@ class FilmController {
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
     @SecurityRequirement(name = "basic")
-    void deleteFilm(@PathVariable Long id) {
+    ResponseEntity<Object> deleteFilm(@PathVariable Long id) {
         var command = new DeleteFilm(id);
         log.info("Command:{}", command);
         deleteFilmHandler.handle(command);
+        var responseEntity = ResponseEntity.noContent().build();
+        log.info("Response entity:{}", responseEntity);
+        return responseEntity;
     }
 
     @GetMapping

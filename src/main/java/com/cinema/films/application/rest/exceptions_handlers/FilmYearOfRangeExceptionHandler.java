8b809971ebc367e677ea888f508e1c4a4e8b8e7d
sampeteq <sampeteq@gmail.com>
2023-11-03 13:52:30 +0100

    Split FilmExceptionHandler into handlers per exception and moved them to "exception_handlers" package

---
+++ src/main/java/com/cinema/films/application/rest/exceptions_handlers/FilmYearOfRangeExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.cinema.films.application.rest.exceptions_handlers;
+
+import com.cinema.films.domain.exceptions.FilmYearOutOfRangeException;
+import com.cinema.shared.exceptions.ExceptionMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class FilmYearOfRangeExceptionHandler {
+
+    @ExceptionHandler(FilmYearOutOfRangeException.class)
+    ResponseEntity<ExceptionMessage> handle(FilmYearOutOfRangeException exception) {
+        var exceptionMessage = new ExceptionMessage(exception.getMessage());
+        return new ResponseEntity<>(exceptionMessage, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+}

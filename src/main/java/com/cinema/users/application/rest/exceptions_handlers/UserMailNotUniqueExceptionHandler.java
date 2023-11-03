4638f9140b52747cbb87ed6d28d53e9e26a0648b
sampeteq <sampeteq@gmail.com>
2023-11-03 14:09:17 +0100

    Split UserExceptionHandler into handlers per exception and moved them to "exception_handlers" package

--- src/main/java/com/cinema/users/application/rest/exceptions_handlers/UserMailNotUniqueExceptionHandler.java
+++ src/main/java/com/cinema/users/application/rest/exceptions_handlers/UserMailNotUniqueExceptionHandler.java
@@ -1,21 +1,14 @@
-package com.cinema.users.application.rest;
+package com.cinema.users.application.rest.exceptions_handlers;
 
 import com.cinema.shared.exceptions.ExceptionMessage;
 import com.cinema.users.domain.exceptions.UserMailNotUniqueException;
-import com.cinema.users.domain.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-class UserExceptionHandler {
-
-    @ExceptionHandler(UserNotFoundException.class)
-    ResponseEntity<ExceptionMessage> handle(UserNotFoundException exception) {
-        var exceptionMessage = new ExceptionMessage(exception.getMessage());
-        return new ResponseEntity<>(exceptionMessage, HttpStatus.NOT_FOUND);
-    }
+class UserMailNotUniqueExceptionHandler {
 
     @ExceptionHandler(UserMailNotUniqueException.class)
     ResponseEntity<ExceptionMessage> handle(UserMailNotUniqueException exception) {

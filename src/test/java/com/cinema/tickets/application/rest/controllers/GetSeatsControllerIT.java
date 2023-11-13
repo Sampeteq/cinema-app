21b33820a73aaf87063b37b8abd44289bc856ec3
sampeteq <sampeteq@gmail.com>
2023-11-13 13:35:32 +0100

    GetSeatsControllerIT fixes and improvements

--- src/test/java/com/cinema/tickets/application/rest/controllers/GetSeatsControllerIT.java
+++ src/test/java/com/cinema/tickets/application/rest/controllers/GetSeatsControllerIT.java
@@ -1,16 +1,10 @@
 package com.cinema.tickets.application.rest.controllers;
 
 import com.cinema.SpringIT;
-import com.cinema.rooms.application.commands.handlers.CreateRoomHandler;
-import com.cinema.screenings.domain.Screening;
-import com.cinema.screenings.domain.ScreeningRepository;
 import com.cinema.tickets.domain.repositories.SeatRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static com.cinema.screenings.ScreeningFixture.SCREENING_DATE;
-import static com.cinema.screenings.ScreeningFixture.createCreateRoomCommand;
-import static com.cinema.screenings.ScreeningFixture.createScreening;
 import static com.cinema.tickets.SeatFixture.createSeats;
 import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.notNullValue;
@@ -20,27 +14,20 @@ class GetSeatsControllerIT extends SpringIT {
     private static final String SEATS_BASE_ENDPOINT = "/seats";
 
     @Autowired
-    private ScreeningRepository screeningRepository;
-
-    @Autowired
     private SeatRepository seatRepository;
-
-    @Autowired
-    private CreateRoomHandler createRoomHandler;
 
     @Test
     void seats_are_gotten_by_screening_id() {
         //given
-        addRoom();
-        var screening = addScreening();
-        addSeats(screening.getId());
+        var screeningId = 1L;
+        addSeats(screeningId);
 
         //when
         var spec = webTestClient
                 .get()
                 .uri(uriBuilder -> uriBuilder
                         .path(SEATS_BASE_ENDPOINT)
-                        .queryParam("screeningId", screening.getId())
+                        .queryParam("screeningId", screeningId)
                         .build()
                 )
                 .exchange();
@@ -59,14 +46,5 @@ class GetSeatsControllerIT extends SpringIT {
 
     private void addSeats(Long screeningId) {
         createSeats(screeningId).forEach(seatRepository::add);
-    }
-
-    private void addRoom() {
-        createRoomHandler.handle(createCreateRoomCommand());
-    }
-
-    private Screening addScreening() {
-        var screening = createScreening(SCREENING_DATE);
-        return screeningRepository.add(screening);
     }
 }

2be736c7bba09462c33cf1796c5d9df1add6198b
sampeteq <sampeteq@gmail.com>
2023-11-13 13:02:54 +0100

    Changed ScreeningCreatedHandler @Component name

--- src/main/java/com/cinema/tickets/application/ScreeningCreatedHandler.java
+++ src/main/java/com/cinema/tickets/application/ScreeningCreatedHandler.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.stream.IntStream;
 
-@Component("Seats - ScreeningCreatedHandler")
+@Component("ScreeningCreatedHandler - seats creation")
 @RequiredArgsConstructor
 public class ScreeningCreatedHandler {
 

31238181488aabb6794786bccf396c91056085fb
sampeteq <sampeteq@gmail.com>
2023-11-16 13:47:00 +0100

    Squashed ticket controllers into one TicketController and moved it from "controllers" package to "rest" package

---
+++ src/main/java/com/cinema/tickets/ui/rest/TicketController.java
@@ -0,0 +1,63 @@
+package com.cinema.tickets.ui.rest;
+
+import com.cinema.tickets.application.commands.BookTicket;
+import com.cinema.tickets.application.commands.CancelTicket;
+import com.cinema.tickets.application.commands.handlers.BookTicketHandler;
+import com.cinema.tickets.application.commands.handlers.CancelTicketHandler;
+import com.cinema.tickets.application.queries.GetAllTicketsByCurrentUser;
+import com.cinema.tickets.application.queries.dto.TicketDto;
+import com.cinema.tickets.application.queries.handlers.GetAllTicketsByCurrentUserHandler;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tickets")
+@RequiredArgsConstructor
+@Slf4j
+class TicketController {
+
+    private final BookTicketHandler bookTicketHandler;
+    private final CancelTicketHandler cancelTicketHandler;
+    private final GetAllTicketsByCurrentUserHandler getAllTicketsByCurrentUserHandler;
+
+    @PostMapping
+    @SecurityRequirement(name = "basic")
+    ResponseEntity<Object> bookTicket(@RequestBody @Valid BookTicket command) {
+        log.info("Command:{}", command);
+        bookTicketHandler.handle(command);
+        var responseEntity = ResponseEntity.created(URI.create("/my/tickets")).build();
+        log.info("Response entity:{}", responseEntity);
+        return responseEntity;
+    }
+
+    @PatchMapping("/{ticketId}/cancel")
+    @SecurityRequirement(name = "basic")
+    ResponseEntity<Object> cancelTicket(@PathVariable Long ticketId) {
+        log.info("Ticket id:{}", ticketId);
+        var command = new CancelTicket(ticketId);
+        cancelTicketHandler.handle(command);
+        var responseEntity = ResponseEntity.ok().build();
+        log.info("Response entity:{}", responseEntity);
+        return responseEntity;
+    }
+
+    @GetMapping("/my")
+    @SecurityRequirement(name = "basic")
+    List<TicketDto> readAllTicketsByCurrentUser() {
+        var query = new GetAllTicketsByCurrentUser();
+        return getAllTicketsByCurrentUserHandler.handle(query);
+    }
+}
--- src/test/java/com/cinema/tickets/ui/rest/TicketControllerIT.java
+++ src/test/java/com/cinema/tickets/ui/rest/TicketControllerIT.java
@@ -1,14 +1,19 @@
-package com.cinema.tickets.ui.rest.controllers;
+package com.cinema.tickets.ui.rest;
 
 import com.cinema.SpringIT;
 import com.cinema.films.application.commands.handlers.CreateFilmHandler;
 import com.cinema.rooms.application.commands.handlers.CreateRoomHandler;
 import com.cinema.screenings.application.commands.handlers.CreateScreeningHandler;
 import com.cinema.tickets.application.commands.BookTicket;
+import com.cinema.tickets.application.queries.dto.TicketDto;
+import com.cinema.tickets.domain.Seat;
 import com.cinema.tickets.domain.SeatStatus;
 import com.cinema.tickets.domain.TicketStatus;
 import com.cinema.tickets.domain.exceptions.SeatAlreadyTakenException;
+import com.cinema.tickets.domain.exceptions.TicketAlreadyCancelledException;
 import com.cinema.tickets.domain.exceptions.TicketBookTooLateException;
+import com.cinema.tickets.domain.exceptions.TicketCancelTooLateException;
+import com.cinema.tickets.domain.repositories.SeatRepository;
 import com.cinema.tickets.domain.repositories.TicketRepository;
 import com.cinema.users.application.commands.CreateUser;
 import com.cinema.users.application.commands.handlers.CreateUserHandler;
@@ -23,16 +28,22 @@ import org.springframework.http.MediaType;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
 
 import static com.cinema.tickets.TicketFixture.SCREENING_DATE;
+import static com.cinema.tickets.TicketFixture.createCancelledTicket;
 import static com.cinema.tickets.TicketFixture.createCreateFilmCommand;
 import static com.cinema.tickets.TicketFixture.createCreateRoomCommand;
 import static com.cinema.tickets.TicketFixture.createCreateScreeningCommand;
+import static com.cinema.tickets.TicketFixture.createSeat;
+import static com.cinema.tickets.TicketFixture.createTicket;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class BookTicketControllerIT extends SpringIT {
+class TicketControllerIT extends SpringIT {
 
     private static final String TICKETS_BASE_ENDPOINT = "/tickets";
     private static final String username = "user1@mail.com";
@@ -40,6 +51,9 @@ class BookTicketControllerIT extends SpringIT {
 
     @Autowired
     private TicketRepository ticketRepository;
+
+    @Autowired
+    private SeatRepository seatRepository;
 
     @Autowired
     private CreateUserHandler createUserHandler;
@@ -223,6 +237,133 @@ class BookTicketControllerIT extends SpringIT {
                 .jsonPath("$.message", equalTo(expectedMessage));
     }
 
+    @Test
+    void ticket_is_cancelled() {
+        //give
+        addScreening();
+        var seat = addSeat();
+        var ticket = ticketRepository.add(createTicket(seat));
+
+        //when
+        var spec = webTestClient
+                .patch()
+                .uri(TICKETS_BASE_ENDPOINT + "/" + 1L + "/cancel")
+                .headers(headers -> headers.setBasicAuth(username, password))
+                .exchange();
+
+        //then
+        spec.expectStatus().isOk();
+        assertThat(ticketRepository.getByIdAndUserId(ticket.getId(), ticket.getUserId()))
+                .isNotEmpty()
+                .hasValueSatisfying(cancelledTicket ->
+                        assertEquals(TicketStatus.CANCELLED, cancelledTicket.getStatus())
+                );
+    }
+
+    @Test
+    void ticket_already_cancelled_cannot_be_cancelled() {
+        //given
+        addScreening();
+        var seat = addSeat();
+        ticketRepository.add(createCancelledTicket(seat));
+
+        //when
+        var spec = webTestClient
+                .patch()
+                .uri(TICKETS_BASE_ENDPOINT + "/" + 1L + "/cancel")
+                .headers(headers -> headers.setBasicAuth(username, password))
+                .exchange();
+
+        //then
+        var expectedMessage = new TicketAlreadyCancelledException().getMessage();
+        spec
+                .expectStatus()
+                .isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+                .expectBody()
+                .jsonPath("$.message", equalTo(expectedMessage));
+    }
+
+    @Test
+    void ticket_is_cancelled_at_least_24h_before_screening() {
+        //given
+        createFilmHandler.handle(createCreateFilmCommand());
+        createRoomHandler.handle(createCreateRoomCommand());
+        var command = createCreateScreeningCommand();
+        createScreeningHandler.handle(command);
+
+        var seat = addSeat();
+        ticketRepository.add(createTicket(seat));
+        Mockito
+                .when(clock.instant())
+                .thenReturn(command.date().minusHours(23).toInstant(ZoneOffset.UTC));
+
+        //when
+        var spec = webTestClient
+                .patch()
+                .uri(TICKETS_BASE_ENDPOINT + "/" + 1L + "/cancel")
+                .headers(headers -> headers.setBasicAuth(username, password))
+                .exchange();
+
+        //then
+        var expectedMessage = new TicketCancelTooLateException().getMessage();
+        spec
+                .expectStatus()
+                .isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+                .expectBody()
+                .jsonPath("$.message", equalTo(expectedMessage));
+    }
+
+    @Test
+    void tickets_are_gotten_by_user_id() {
+        //given
+        var createFilmCommand = createCreateFilmCommand();
+        createFilmHandler.handle(createFilmCommand);
+
+        var createRoomCommand = createCreateRoomCommand();
+        createRoomHandler.handle(createRoomCommand);
+
+        var createScreeningCommand = createCreateScreeningCommand();
+        createScreeningHandler.handle(createScreeningCommand);
+
+        var seat = addSeat();
+        var ticket = ticketRepository.add(createTicket(seat));
+
+        var rowNumber = 1;
+        var seatNumber = 1;
+
+        //when
+        var spec = webTestClient
+                .get()
+                .uri(TICKETS_BASE_ENDPOINT + "/my")
+                .headers(headers -> headers.setBasicAuth(username, password))
+                .exchange();
+
+        //then
+        var expected = List.of(
+                new TicketDto(
+                        1L,
+                        ticket.getStatus(),
+                        createFilmCommand.title(),
+                        createScreeningCommand.date(),
+                        createRoomCommand.id(),
+                        rowNumber,
+                        seatNumber
+                )
+        );
+        spec
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .jsonPath("$[*]").value(everyItem(notNullValue()))
+                .jsonPath("$[0].id").isEqualTo(expected.get(0).id())
+                .jsonPath("$[0].status").isEqualTo(expected.get(0).status().name())
+                .jsonPath("$[0].filmTitle").isEqualTo(expected.get(0).filmTitle())
+                .jsonPath("$[0].screeningDate").isEqualTo(expected.get(0).screeningDate().toString())
+                .jsonPath("$[0].roomId").isEqualTo(expected.get(0).roomId())
+                .jsonPath("$[0].rowNumber").isEqualTo(expected.get(0).rowNumber())
+                .jsonPath("$[0].seatNumber").isEqualTo(expected.get(0).seatNumber());
+    }
+
     private void addScreening() {
         createFilmHandler.handle(createCreateFilmCommand());
         createRoomHandler.handle(createCreateRoomCommand());
@@ -239,6 +380,10 @@ class BookTicketControllerIT extends SpringIT {
         createFilmHandler.handle(createCreateFilmCommand(filmTitle));
         createRoomHandler.handle(createCreateRoomCommand(roomId));
         createScreeningHandler.handle(createCreateScreeningCommand(SCREENING_DATE));
+    }
+
+    private Seat addSeat() {
+        return seatRepository.add(createSeat());
     }
 
     private void bookTicket(long screeningId, int rowNumber, int seatNumber) {

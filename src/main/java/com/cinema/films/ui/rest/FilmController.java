0a0fd95d897597dc1e2c5e9b70261c61c75aef2d
sampeteq <sampeteq@gmail.com>
2023-11-16 13:25:43 +0100

    Squashed film controllers into one FilmController and moved it from "controllers" package to "rest" package

---
+++ src/main/java/com/cinema/films/ui/rest/FilmController.java
@@ -0,0 +1,71 @@
+package com.cinema.films.ui.rest;
+
+import com.cinema.films.application.commands.CreateFilm;
+import com.cinema.films.application.commands.DeleteFilm;
+import com.cinema.films.application.commands.handlers.CreateFilmHandler;
+import com.cinema.films.application.commands.handlers.DeleteFilmHandler;
+import com.cinema.films.application.queries.GetFilms;
+import com.cinema.films.application.queries.dto.FilmDto;
+import com.cinema.films.application.queries.handlers.GetFilmsHandler;
+import com.cinema.films.domain.FilmCategory;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/films")
+@RequiredArgsConstructor
+@Slf4j
+public class FilmController {
+
+    private final CreateFilmHandler createFilmHandler;
+    private final DeleteFilmHandler deleteFilmHandler;
+    private final GetFilmsHandler getFilmsHandler;
+
+    @PostMapping
+    @SecurityRequirement(name = "basic")
+    ResponseEntity<Object> createFilm(@RequestBody @Valid CreateFilm command) {
+        log.info("Command:{}", command);
+        createFilmHandler.handle(command);
+        var responseEntity = ResponseEntity.created(URI.create("/films")).build();
+        log.info("Response entity{}", responseEntity);
+        return responseEntity;
+    }
+
+    @DeleteMapping("/{id}")
+    @SecurityRequirement(name = "basic")
+    ResponseEntity<Object> deleteFilm(@PathVariable Long id) {
+        var command = new DeleteFilm(id);
+        log.info("Command:{}", command);
+        deleteFilmHandler.handle(command);
+        var responseEntity = ResponseEntity.noContent().build();
+        log.info("Response entity:{}", responseEntity);
+        return responseEntity;
+    }
+
+    @GetMapping
+    List<FilmDto> readAllFilms(
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) FilmCategory category
+    ) {
+        var queryDto = GetFilms
+                .builder()
+                .title(title)
+                .category(category)
+                .build();
+        return getFilmsHandler.handle(queryDto);
+    }
+}
--- src/test/java/com/cinema/films/ui/rest/FilmControllerIT.java
+++ src/test/java/com/cinema/films/ui/rest/FilmControllerIT.java
@@ -1,4 +1,4 @@
-package com.cinema.films.ui.rest.controllers;
+package com.cinema.films.ui.rest;
 
 import com.cinema.SpringIT;
 import com.cinema.films.application.commands.CreateFilm;
@@ -16,14 +16,17 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static com.cinema.films.FilmFixture.createCreateFilmCommand;
 import static com.cinema.films.FilmFixture.createFilm;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CreateFilmControllerIT extends SpringIT {
+class FilmControllerIT extends SpringIT {
 
     private static final String FILMS_BASE_ENDPOINT = "/films";
     private static final String USERNAME = "user";
@@ -141,6 +144,112 @@ class CreateFilmControllerIT extends SpringIT {
                 .isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
                 .expectBody()
                 .jsonPath("$.message", equalTo(expectedMessage));
+    }
+
+    @Test
+    void film_is_deleted_only_by_admin() {
+        //given
+        addCommonUser();
+
+        //when
+        var spec = webTestClient
+                .delete()
+                .uri(FILMS_BASE_ENDPOINT + "/Film 1")
+                .headers(headers -> headers.setBasicAuth(USERNAME, PASSWORD))
+                .exchange();
+
+        //then
+        spec.expectStatus().isForbidden();
+    }
+
+    @Test
+    @WithMockUser(authorities = "ADMIN")
+    void film_is_deleted() {
+        //given
+        addAdminUser();
+        var film = filmRepository.add(createFilm());
+
+        //when
+        var spec = webTestClient
+                .delete()
+                .uri(FILMS_BASE_ENDPOINT + "/" + film.getId())
+                .headers(headers -> headers.setBasicAuth(USERNAME, PASSWORD))
+                .exchange();
+
+        //then
+        spec.expectStatus().isNoContent();
+        assertThat(filmRepository.existsByTitle(film.getTitle())).isFalse();
+    }
+
+    @Test
+    void films_are_gotten() {
+        //given
+        var film = filmRepository.add(createFilm());
+
+        //when
+        var spec = webTestClient
+                .get()
+                .uri(FILMS_BASE_ENDPOINT)
+                .exchange();
+
+        //then
+        spec
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .jsonPath("$[*]").value(hasSize(1))
+                .jsonPath("$[0].title").isEqualTo(film.getTitle())
+                .jsonPath("$[0].category").isEqualTo(film.getCategory().name())
+                .jsonPath("$[0].year").isEqualTo(film.getYear())
+                .jsonPath("$[0].durationInMinutes").isEqualTo(film.getDurationInMinutes());
+    }
+
+    @Test
+    void films_are_gotten_by_title() {
+        //given
+        var title = "Film";
+        var otherTitle = "Other Film";
+        filmRepository.add(createFilm(title));
+        filmRepository.add(createFilm(otherTitle));
+
+        //when
+        var spec = webTestClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(FILMS_BASE_ENDPOINT)
+                        .queryParam("title", title)
+                        .build()
+                )
+                .exchange();
+
+        //then
+        spec
+                .expectBody()
+                .jsonPath("$.*.title").value(everyItem(equalTo(title)));
+    }
+
+    @Test
+    void films_are_gotten_by_category() {
+        //given
+        var category = FilmCategory.COMEDY;
+        var otherCategory = FilmCategory.DRAMA;
+        filmRepository.add(createFilm(category));
+        filmRepository.add(createFilm(otherCategory));
+
+        //when
+        var spec = webTestClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(FILMS_BASE_ENDPOINT)
+                        .queryParam("category", category)
+                        .build()
+                )
+                .exchange();
+
+        //then
+        spec
+                .expectBody()
+                .jsonPath("$.*.category").value(everyItem(equalTo(category.name())));
     }
 
     private void addCommonUser() {

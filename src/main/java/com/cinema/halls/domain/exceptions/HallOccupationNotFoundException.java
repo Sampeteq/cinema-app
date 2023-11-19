08387ea68380b2cbd7d3a94452dc6c4cec6b5065
sampeteq <sampeteq@gmail.com>
2023-11-19 13:01:06 +0100

    Removed useless exception

--- src/main/java/com/cinema/halls/domain/exceptions/HallOccupationNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.cinema.halls.domain.exceptions;
-
-public class HallOccupationNotFoundException extends RuntimeException {
-
-    public HallOccupationNotFoundException() {
-        super("Hall occupation not found");
-    }
-}

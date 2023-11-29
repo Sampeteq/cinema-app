eaf723ad37193e3d67aa8ef0ffa89f9754cfb5e9
sampeteq <sampeteq@gmail.com>
2023-11-29 15:22:03 +0100

    Squashed User exceptions handling classes into one UserExceptionHandler

--- src/main/java/com/cinema/users/ui/rest/UserExceptionHandler.java
+++ src/main/java/com/cinema/users/ui/rest/UserExceptionHandler.java
@@ -1,6 +1,7 @@
-package com.cinema.users.ui.rest.exceptions_handlers;
+package com.cinema.users.ui.rest;
 
 import com.cinema.shared.exceptions.ExceptionMessage;
+import com.cinema.users.domain.exceptions.UserMailNotUniqueException;
 import com.cinema.users.domain.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-class UserNotFoundExceptionHandler {
+class UserExceptionHandler {
+
+    @ExceptionHandler
+    ResponseEntity<ExceptionMessage> handle(UserMailNotUniqueException exception) {
+        var exceptionMessage = new ExceptionMessage(exception.getMessage());
+        return new ResponseEntity<>(exceptionMessage, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
 
     @ExceptionHandler
     ResponseEntity<ExceptionMessage> handle(UserNotFoundException exception) {

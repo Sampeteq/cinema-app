53df0ad9b07007ed70592266024ed3e801eaed66
sampeteq <sampeteq@gmail.com>
2023-11-29 14:54:06 +0100

    Squashed Film exceptions handling classes into one FilmExceptionHandler

---
+++ src/main/java/com/cinema/films/ui/rest/FilmExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.cinema.films.ui.rest;
+
+import com.cinema.films.domain.exceptions.FilmNotFoundException;
+import com.cinema.films.domain.exceptions.FilmTitleNotUniqueException;
+import com.cinema.films.domain.exceptions.FilmYearOutOfRangeException;
+import com.cinema.shared.exceptions.ExceptionMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+class FilmExceptionHandler {
+
+    @ExceptionHandler
+    ResponseEntity<ExceptionMessage> handle(FilmNotFoundException exception) {
+        var exceptionMessage = new ExceptionMessage(exception.getMessage());
+        return new ResponseEntity<>(exceptionMessage, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    ResponseEntity<ExceptionMessage> handle(FilmTitleNotUniqueException exception) {
+        var exceptionMessage = new ExceptionMessage(exception.getMessage());
+        return new ResponseEntity<>(exceptionMessage, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler
+    ResponseEntity<ExceptionMessage> handle(FilmYearOutOfRangeException exception) {
+        var exceptionMessage = new ExceptionMessage(exception.getMessage());
+        return new ResponseEntity<>(exceptionMessage, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+}

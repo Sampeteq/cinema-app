33198812c0096f0148ccf2951e62184c872c7631
sampeteq <sampeteq@gmail.com>
2023-12-03 15:30:02 +0100

    Used JDBCTemplate instead of repository in getting hall occupations and removed HallOccupationRepository and HallOccupationMapper

--- src/main/java/com/cinema/halls/application/queries/handlers/GetAllHallOccupationsHandler.java
+++ src/main/java/com/cinema/halls/application/queries/handlers/GetAllHallOccupationsHandler.java
@@ -2,12 +2,12 @@ package com.cinema.halls.application.queries.handlers;
 
 import com.cinema.halls.application.queries.GetAllHallOccupations;
 import com.cinema.halls.application.queries.dto.HallOccupationDto;
-import com.cinema.halls.application.queries.dto.HallOccupationMapper;
-import com.cinema.halls.domain.HallOccupationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -15,15 +15,18 @@ import java.util.List;
 @Slf4j
 public class GetAllHallOccupationsHandler {
 
-    private final HallOccupationRepository hallOccupationRepository;
-    private final HallOccupationMapper hallOccupationMapper;
+    private final JdbcTemplate jdbcTemplate;
 
     public List<HallOccupationDto> handle(GetAllHallOccupations query) {
       log.info("Query:{}", query);
-      return hallOccupationRepository
-              .getAll()
-              .stream()
-              .map(hallOccupationMapper::mapToDto)
-              .toList();
+      return jdbcTemplate.query(
+              "select h.start_at, h.end_at, h.hall_id, h.screening_id from halls_occupations h",
+              (resultSet, rowNumber) -> new HallOccupationDto(
+                      resultSet.getObject("start_at", LocalDateTime.class),
+                      resultSet.getObject("end_at", LocalDateTime.class),
+                      resultSet.getLong("hall_id"),
+                      resultSet.getLong("screening_id")
+              )
+      );
     }
 }
--- src/main/java/com/cinema/halls/domain/HallOccupationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cinema.halls.domain;
-
-import java.util.List;
-
-public interface HallOccupationRepository {
-    List<HallOccupation> getAll();
-}

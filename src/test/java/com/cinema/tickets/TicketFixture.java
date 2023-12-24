93f6588b9a2732f7254d50b7464073c573c5ec87
sampeteq <sampeteq@gmail.com>
2023-12-24 02:38:48 +0100

    TicketFixture fixes

--- src/test/java/com/cinema/tickets/TicketFixture.java
+++ src/test/java/com/cinema/tickets/TicketFixture.java
@@ -8,14 +8,11 @@ import java.time.temporal.ChronoUnit;
 
 public final class TicketFixture {
     public static final Long SCREENING_ID = 1L;
-    public static final Long HALL_ID = 1L;
     public static final LocalDateTime SCREENING_DATE = LocalDateTime
             .now()
             .plusDays(8)
             .truncatedTo(ChronoUnit.MINUTES);
     public static final Long SEAT_ID = 1L;
-    public static final int SEAT_ROW_NUMBER = 1;
-    public static final int SEAT_NUMBER = 1;
     public static final long USER_ID = 1L;
 
     private TicketFixture() {

739756be32549552857a83883e25388c614d8ca2
sampeteq <sampeteq@gmail.com>
2024-01-01 14:20:21 +0100

    Changed AdminProperties class to record and moved it to AdminInitializer

--- src/main/java/com/cinema/SpringMain.java
+++ src/main/java/com/cinema/SpringMain.java
@@ -2,9 +2,11 @@ package com.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableScheduling
 class SpringMain {
 
--- src/main/java/com/cinema/users/infrastrcuture/AdminInitializer.java
+++ src/main/java/com/cinema/users/infrastrcuture/AdminInitializer.java
@@ -4,6 +4,7 @@ import com.cinema.users.application.commands.CreateUser;
 import com.cinema.users.application.commands.handlers.CreateAdminHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -20,7 +21,9 @@ class AdminInitializer {
 
     @EventListener(ContextRefreshedEvent.class)
     void createAdminOnStartUp() {
-        var command = new CreateUser(adminProperties.getMail(), adminProperties.getPassword());
+        var command = new CreateUser(adminProperties.mail(), adminProperties.password());
         createAdminHandler.handle(command);
     }
 }
+@ConfigurationProperties(prefix = "admin")
+record AdminProperties(String mail, String password) {}

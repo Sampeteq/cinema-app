19a2c2017176f5b9bdb43f5aeb12caaf96ec992e
sampeteq <sampeteq@gmail.com>
2024-01-01 14:22:18 +0100

    AdminInitializer fixes

--- src/main/java/com/cinema/users/infrastrcuture/AdminInitializer.java
+++ src/main/java/com/cinema/users/infrastrcuture/AdminInitializer.java
@@ -3,17 +3,15 @@ package com.cinema.users.infrastrcuture;
 import com.cinema.users.application.commands.CreateUser;
 import com.cinema.users.application.commands.handlers.CreateAdminHandler;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @Profile("prod")
 @RequiredArgsConstructor
-@Slf4j
 class AdminInitializer {
 
     private final AdminProperties adminProperties;

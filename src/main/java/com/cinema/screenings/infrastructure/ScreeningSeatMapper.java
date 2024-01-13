d6f3a89e59584df7eed2a9efd0f9ff505485c03a
sampeteq <sampeteq@gmail.com>
2024-01-13 15:08:50 +0100

    Fixed ScreeningSeatMapper

--- src/main/java/com/cinema/screenings/infrastructure/ScreeningSeatMapper.java
+++ src/main/java/com/cinema/screenings/infrastructure/ScreeningSeatMapper.java
@@ -3,8 +3,12 @@ package com.cinema.screenings.infrastructure;
 import com.cinema.screenings.application.dto.ScreeningSeatDto;
 import com.cinema.screenings.domain.ScreeningSeat;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface ScreeningSeatMapper {
+    @Mapping(source = "hallSeat.rowNumber", target = "rowNumber")
+    @Mapping(source = "hallSeat.number", target = "number")
+    @Mapping(source = "free", target = "isFree")
     ScreeningSeatDto mapToDto(ScreeningSeat seat);
 }

fc2c0ca8cbf0b3d87f2fc8fb9c243c6495ad2dd1
sampeteq <sampeteq@gmail.com>
2024-01-28 17:03:13 +0100

    Removed useless logging from TicketController

--- src/main/java/com/cinema/tickets/ui/TicketController.java
+++ src/main/java/com/cinema/tickets/ui/TicketController.java
@@ -5,8 +5,6 @@ import com.cinema.tickets.application.dto.BookTicketDto;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,29 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/tickets")
 @RequiredArgsConstructor
-@Slf4j
 class TicketController {
 
     private final TicketService ticketService;
 
     @PostMapping
     @SecurityRequirement(name = "basic")
-    ResponseEntity<Object> bookTicket(@RequestBody @Valid BookTicketDto dto) {
-        log.info("Dto:{}", dto);
+    void bookTicket(@RequestBody @Valid BookTicketDto dto) {
         ticketService.bookTicket(dto);
-        var responseEntity = ResponseEntity.ok().build();
-        log.info("Response entity:{}", responseEntity);
-        return responseEntity;
     }
 
     @PatchMapping("/{ticketId}/cancel")
     @SecurityRequirement(name = "basic")
-    ResponseEntity<Object> cancelTicket(@PathVariable Long ticketId) {
-        log.info("Ticket id:{}", ticketId);
+    void cancelTicket(@PathVariable Long ticketId) {
         ticketService.cancelTicket(ticketId);
-        var responseEntity = ResponseEntity.ok().build();
-        log.info("Response entity:{}", responseEntity);
-        return responseEntity;
     }
 
     @GetMapping("/my")

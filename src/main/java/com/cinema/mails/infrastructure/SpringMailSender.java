86e72386491595ec9b11874e77ee5d236b251878
sampeteq <sampeteq@gmail.com>
2024-02-01 20:15:47 +0100

    SpringMailSender fixes

--- src/main/java/com/cinema/mails/infrastructure/SpringMailSender.java
+++ src/main/java/com/cinema/mails/infrastructure/SpringMailSender.java
@@ -3,21 +3,21 @@ package com.cinema.mails.infrastructure;
 import com.cinema.mails.domain.MailMessage;
 import com.cinema.mails.domain.MailService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.Clock;
 
 @Service
-@Profile("prod")
 @RequiredArgsConstructor
 class SpringMailSender implements MailService {
 
     private final JavaMailSender javaMailSender;
     private final Clock clock;
 
+    @Async
     @Override
     public void sendMail(MailMessage mailMessage) {
         var message = new SimpleMailMessage();

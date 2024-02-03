2ca91346be3395131b5607ddde9095554e3aff77
sampeteq <sampeteq@gmail.com>
2024-02-03 23:48:04 +0100

    Removed FilmFactory and moved its logic to FilmService

--- src/main/java/com/cinema/films/application/FilmService.java
+++ src/main/java/com/cinema/films/application/FilmService.java
@@ -3,9 +3,9 @@ package com.cinema.films.application;
 import com.cinema.films.application.dto.AddFilmDto;
 import com.cinema.films.domain.Film;
 import com.cinema.films.domain.FilmCategory;
-import com.cinema.films.domain.FilmFactory;
 import com.cinema.films.domain.FilmRepository;
 import com.cinema.films.domain.exceptions.FilmNotFoundException;
+import com.cinema.films.domain.exceptions.FilmTitleNotUniqueException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,12 +17,14 @@ import java.util.List;
 @Slf4j
 public class FilmService {
 
-    private final FilmFactory filmFactory;
     private final FilmRepository filmRepository;
 
     public Film addFilm(AddFilmDto dto) {
         log.info("Dto:{}", dto);
-        var film = filmFactory.createFilm(
+        if (filmRepository.findByTitle(dto.title()).isPresent()) {
+            throw new FilmTitleNotUniqueException();
+        }
+        var film = new Film(
                 dto.title(),
                 dto.category(),
                 dto.year(),

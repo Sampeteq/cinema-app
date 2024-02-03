8bf7062c8f301a8c12e24d21e964da99c57ae85e
sampeteq <sampeteq@gmail.com>
2024-02-03 18:26:08 +0100

    Optimized imports

--- src/main/java/com/cinema/mails/MailService.java
+++ src/main/java/com/cinema/mails/MailService.java
@@ -3,7 +3,6 @@ package com.cinema.mails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.Clock;

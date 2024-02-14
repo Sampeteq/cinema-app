7495180e767fb8afab2102009d7223dd528085f4
sampeteq <sampeteq@gmail.com>
2024-02-14 16:30:03 +0100

    Fixed message in TicketAlreadyBookedException

--- src/main/java/com/cinema/tickets/domain/exceptions/TicketAlreadyBookedException.java
+++ src/main/java/com/cinema/tickets/domain/exceptions/TicketAlreadyBookedException.java
@@ -3,6 +3,6 @@ package com.cinema.tickets.domain.exceptions;
 public class TicketAlreadyBookedException extends RuntimeException {
 
     public TicketAlreadyBookedException() {
-        super("Ticket already exists");
+        super("Ticket already booked");
     }
 }

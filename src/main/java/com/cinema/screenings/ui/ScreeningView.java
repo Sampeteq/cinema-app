4acf58b98641223d37eef29bc22923d0691cd286
sampeteq <sampeteq@gmail.com>
2024-02-17 17:46:07 +0100

    Made ScreeningView package private

--- src/main/java/com/cinema/screenings/ui/ScreeningView.java
+++ src/main/java/com/cinema/screenings/ui/ScreeningView.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
-public record ScreeningView(
+record ScreeningView(
         Long id,
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
         LocalDateTime date,

9343442d8b3030417b2610ec74b549949ac16d78
sampeteq <sampeteq@gmail.com>
2024-02-17 16:54:08 +0100

    Removed @EnableScheduling annotation from SpringMain

--- src/main/java/com/cinema/SpringMain.java
+++ src/main/java/com/cinema/SpringMain.java
@@ -3,11 +3,9 @@ package com.cinema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableScheduling
 class SpringMain {
 
     public static void main(String[] args) {

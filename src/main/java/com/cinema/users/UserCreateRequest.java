12c5fd36aa48758d3b6484a444d9b1fa092eb9a1
sampeteq <sampeteq@gmail.com>
2024-02-22 21:25:32 +0100

    Made UserCreateRequest package private

--- src/main/java/com/cinema/users/UserCreateRequest.java
+++ src/main/java/com/cinema/users/UserCreateRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record UserCreateRequest(
+record UserCreateRequest(
         @NotBlank
         @Email
         String mail,

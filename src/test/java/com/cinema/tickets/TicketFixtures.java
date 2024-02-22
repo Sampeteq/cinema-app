4b4c8666ab7874416984f7adeb2884893b0c3966
sampeteq <sampeteq@gmail.com>
2024-02-22 22:00:40 +0100

    TicketFixtures improvements

--- src/test/java/com/cinema/tickets/TicketFixtures.java
+++ src/test/java/com/cinema/tickets/TicketFixtures.java
@@ -3,18 +3,15 @@ package com.cinema.tickets;
 import com.cinema.halls.Seat;
 import com.cinema.screenings.Screening;
 
-public final class TicketFixtures {
+class TicketFixtures {
 
-    public static final Seat SEAT = new Seat(1, 1);
+    static final Seat SEAT = new Seat(1, 1);
 
-    private TicketFixtures() {
-    }
-
-    public static Ticket createTicket(Screening screening) {
+    static Ticket createTicket(Screening screening) {
         return new Ticket(screening, SEAT);
     }
 
-    public static Ticket createTicket(Screening screening, Long userId) {
+    static Ticket createTicket(Screening screening, Long userId) {
         return new Ticket(screening, SEAT, userId);
     }
 }

797dc251f5b0b818490324e14043783921393f56
sampeteq <sampeteq@gmail.com>
2024-02-26 20:02:48 +0100

    ClockFixtures fixes

--- src/test/java/com/cinema/ClockFixtures.java
+++ src/test/java/com/cinema/ClockFixtures.java
@@ -1,19 +1,13 @@
 package com.cinema;
 
-import java.time.Clock;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 public class ClockFixtures {
-
     public static final ZoneOffset ZONE_OFFSET = ZoneOffset.UTC;
 
-    public static final Instant INSTANT = LocalDateTime
-            .of(2024, 1, 1, 0,0)
-            .toInstant(ZONE_OFFSET);
+    public static final LocalDateTime CURRENT_DATE = LocalDateTime.of(2024, 1, 1, 0, 0);
 
-    public static final Clock CLOCK = Clock.fixed(INSTANT, ZONE_OFFSET);
-
-    public static final LocalDateTime CURRENT_DATE = LocalDateTime.now(CLOCK);
+    public static final Instant INSTANT = CURRENT_DATE.toInstant(ZONE_OFFSET);
 }

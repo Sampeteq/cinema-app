2bb9d1a3876f76a20b6a331f81a75731cfe16b2c
sampeteq <sampeteq@gmail.com>
2024-02-28 10:30:17 +0100

    Removed useless custom query from TicketRepository

--- src/main/java/com/cinema/tickets/TicketRepository.java
+++ src/main/java/com/cinema/tickets/TicketRepository.java
@@ -2,32 +2,16 @@ package com.cinema.tickets;
 
 import com.cinema.halls.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 interface TicketRepository extends JpaRepository<Ticket, Long> {
 
-    @Query("""
-                from Ticket ticket
-                left join fetch ticket.seat seat
-                where seat = :seat
-            """)
     Optional<Ticket> findBySeat(Seat seat);
 
-    @Query("""
-            from Ticket ticket
-            left join fetch ticket.seat seat
-            where ticket.id = :ticketId and ticket.userId = :userId
-            """)
     Optional<Ticket> findByIdAndUserId(Long ticketId, Long userId);
 
-    @Query("""
-            from Ticket ticket
-            left join fetch ticket.seat seat
-            where ticket.userId = :userId
-            """)
     List<Ticket> findAllByUserId(Long userId);
 
     List<Ticket> findAllByScreeningId(Long screeningId);

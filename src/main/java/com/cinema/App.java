a87e05f988a4dedb05f199ef73a9ec2be24c6537
sampeteq <sampeteq@gmail.com>
2024-03-05 22:19:41 +0100

    Enabled async

--- src/main/java/com/cinema/App.java
+++ src/main/java/com/cinema/App.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableJpaRepositories(considerNestedRepositories = true)
+@EnableAsync
 class App {
 
     public static void main(String[] args) {
--- src/main/java/com/cinema/mails/MailService.java
+++ src/main/java/com/cinema/mails/MailService.java
@@ -3,6 +3,7 @@ package com.cinema.mails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class MailService {
 
     private final JavaMailSender javaMailSender;
 
+    @Async
     public void sendMail(String receiver, String subject, String text) {
         var message = new SimpleMailMessage();
         message.setTo(receiver);

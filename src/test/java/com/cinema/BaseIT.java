bc9ac2d2a94aa39c3f94605d220fbb41cba73590
sampeteq <sampeteq@gmail.com>
2024-03-26 21:17:24 +0100

    Add Spring Boot Testcontainers

--- src/test/java/com/cinema/BaseIT.java
+++ src/test/java/com/cinema/BaseIT.java
@@ -7,8 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 
@@ -22,6 +21,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class BaseIT {
 
+    @ServiceConnection
     private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(
             "postgres:latest"
     );
@@ -41,13 +41,6 @@ public abstract class BaseIT {
 
     @SpyBean
     private Clock clock;
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresContainer::getUsername);
-        registry.add("spring.datasource.password", postgresContainer::getPassword);
-    }
 
     @BeforeEach
     protected void setUpMocks() {
